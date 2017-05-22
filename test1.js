ac1579bf70602b00d99303ef0cd23fe7605b8109
陈凯 <chenkai@zuoyebang.com>
2017-05-22 16:02:39 +0800

    [保存图片，可视窗口参数设置]

--- test1.js
+++ test1.js
@@ -4,8 +4,8 @@ var webPage = require('webpage'),
   page = webPage.create();
 
 page.viewportSize = {
-  width: 1920,
-  height: 1080
+  width: 375,
+  height: 667
 };
 
 page.settings.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
